981dabf264c075e3446cae25222751f39ff502bf
EtienneVassallo-Huet <ivan.tixier@ecam.fr>
2022-04-02 11:38:44 +0200

    Add current user to db collection ISBN for loading all the books of the current user

--- routes/dashboard.py
+++ routes/dashboard.py
@@ -3,6 +3,7 @@ from utility.wraps import require_login
 from flask_pymongo import PyMongo
 import json
 
+
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/readingcorner"
 mongo = PyMongo(app)
@@ -18,6 +19,8 @@ def dashboard():
 def test(): 
     output = request.get_json()
     result = json.loads(output)
+    userid = request.cookies.get("userID")
+    print(userid)
     print(result)
-    mongo.db.isbn.insert_one({"result":result})
+    mongo.db.isbn.insert_one({"username":userid,"result":result})
     return result

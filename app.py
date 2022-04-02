53a2e58972dfc889bef37588c48c2e7cec093260
Etienne Vassallo <etienne.vassallo@icloud.com>
2022-04-02 12:18:19 +0200

    Merge branch 'master' of https://github.com/itmo-wad/ReadingCorner-Team7

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ from main import app
 
 if __name__ == "__main__":
     # TODO Remove host and port once hosted on VPS
-    app.run(host="localhost", port=5000, debug=True)
+    app.run(host="localhost", port=5003, debug=True)
--- routes/dashboard.py
+++ routes/dashboard.py
@@ -2,6 +2,7 @@ from flask import Flask, Blueprint, redirect, url_for, render_template, request,
 from utility.wraps import require_login
 from flask_pymongo import PyMongo
 import json
+import requests
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/readingcorner"
@@ -20,4 +21,10 @@ def test():
     result = json.loads(output)
     print(result)
     mongo.db.isbn.insert_one({"result":result})
+    data = requests.get('https://www.googleapis.com/books/v1/volumes?q=isbn:' + str(result['bookIsbn']))
+    infos = data.text
+    infos_dict = json.loads(infos)
+    infos2 = infos_dict['items'][0]
+    #infos2_dict = json.loads(infos2)
+    print(infos2['volumeInfo']['title'])
     return result

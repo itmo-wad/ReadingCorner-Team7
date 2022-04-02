9f25c4e16c0b5fff5b2cbcb37a8f6589d974b642
EtienneVassallo-Huet <ivan.tixier@ecam.fr>
2022-04-02 10:04:52 +0200

    Regulate conflict of Etienne version

--- routes/dashboard.py
+++ routes/dashboard.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Flask, Blueprint, redirect, url_for, render_template, request, flash
 from utility.wraps import require_login
 from flask_pymongo import PyMongo
@@ -7,17 +6,12 @@ import json
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/wad"
 mongo = PyMongo(app)
-=======
-from flask import Blueprint, redirect, url_for, render_template, request, flash
-from utility.wraps import require_login
->>>>>>> 296fa7e94821ebcd403ceb0a7fd52caca3d545b2
 
 dashboard_page = Blueprint('dashboard_page', __name__, template_folder='templates')
 
 @dashboard_page.route("/dashboard")
 @require_login
 def dashboard():
-<<<<<<< HEAD
     return render_template('dashboard.html', stylesheets=["/static/css/main.css"])
 
 @dashboard_page.route('/test', methods=['POST']) 
@@ -27,6 +21,3 @@ def test():
     print(result)
     mongo.db.isbn.insert_one({"result":result})
     return result
-=======
-    return render_template('dashboard.html', stylesheets=["/static/css/main.css"])
->>>>>>> 296fa7e94821ebcd403ceb0a7fd52caca3d545b2

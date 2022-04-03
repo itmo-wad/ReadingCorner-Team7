6220c63e4420999e2ac9534437efb9ecbd289369
EtienneVassallo-Huet <ivan.tixier@ecam.fr>
2022-04-03 12:45:45 +0200

    Clarification données dans database et add title field

--- routes/dashboard.py
+++ routes/dashboard.py
@@ -32,7 +32,7 @@ def dashboard():
                     {"cover":"/static/img/cover-TEST.jpg", "title":"Comedy 101", "author":"Rowan Atkinson", "link": "https://www.google.fr/books/edition/Blue_Highways/DTk3AQAAQBAJ?hl=en&gbpv=1"},
                     {"cover":"/static/img/cover-TEST.jpg", "title":"The way of nameless", "author":"Graham Douglass", "link": "https://www.google.fr/books/edition/Blue_Highways/DTk3AQAAQBAJ?hl=en&gbpv=1"}]
 
-    return render_template('louis-dashboard.html', stylesheets=["https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
+    return render_template('dashboard.html', stylesheets=["https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
         "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css",
         "/static/css/main.css", "/static/css/dashboard.css"], releases=new_releases, books=my_books,thumbnailBooks=thumbnailBooks)
 
@@ -42,29 +42,35 @@ def test():
     result = json.loads(output)
     userid = request.cookies.get("userID")
     print(userid)
-    print(result)
-    mongo.db.isbn.insert_one({"username":userid,"result":result})
+    print("Le result est",result)
     data = requests.get('https://www.googleapis.com/books/v1/volumes?q=isbn:' + str(result['bookIsbn']))
     infos = data.text
     infos_dict = json.loads(infos)
-    infos2 = infos_dict['items'][0]
+    title = infos_dict['items'][0]['volumeInfo']['title']
+    mongo.db.isbn.insert_one({"username":userid,"ISBN":result['bookIsbn'],"title":title})
+    
+    
     #infos2_dict = json.loads(infos2)
     #print(infos2['volumeInfo']['title'])
     return result
 
+
+@dashboard_page.route('/remove', methods=['POST']) 
+def removeBook():
+    print("remove book")
+    return redirect('/dashboard')
 
 # Function that takes the list of books of the current user, extract the ISBN from the database and do a request to the API with this ISBN. 
 # As a result, we can extract the different parameters for the book in question.
 def printbooks(listbooks):
     imageBooks=[]
     for i in range (len(listbooks)):
-        result=listbooks[i]['result']['bookIsbn']
+        result=listbooks[i]['ISBN']
         data = requests.get('https://www.googleapis.com/books/v1/volumes?q=isbn:' + str(result))
         infos = data.text
         #print(infos)
         infos_dict = json.loads(infos)
         infos2 = infos_dict['items'][0]
-        #infos2_dict = json.loads(infos2)
         #print(infos2['volumeInfo']['title'])
         #print(infos2['volumeInfo'].get("imageLinks"))
         if infos2['volumeInfo'].get("imageLinks")==None:
